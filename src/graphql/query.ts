b305bfaf8dc813a497c86574276980856b2153ab
SoufianeKnadry <Soufianeknad@gmail.com>
2025-02-16 16:05:21 -0500

    Add option to get all completed/incompleted todos

--- src/graphql/query.ts
+++ src/graphql/query.ts
@@ -2,15 +2,18 @@ import { type QueryResolvers as IQuery } from "./generated/graphql";
 import { Context } from "./context";
 
 export const Query: IQuery<Context> = {
-  getTodos: async (_, __, { prisma }) => {
-    const todos = await prisma.todo.findMany();
-    
+  getTodos: async (_, { completed }, { prisma }) => {
+    const todos = await prisma.todo.findMany({
+      where: completed != null ? { completed } : {},
+    });
+
     return todos.map(todo => ({
       ...todo,
-      createdAt: todo.createdAt.toISOString(),  
-      updatedAt: todo.updatedAt.toISOString(), 
+      createdAt: todo.createdAt.toISOString(),
+      updatedAt: todo.updatedAt.toISOString(),
     }));
   },
+
 
   getTodo: async (_, { id }, { prisma }) => {
     const todo = await prisma.todo.findUnique({
