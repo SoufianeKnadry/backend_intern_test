215492f77b7af21aa3c3f2186dbbeb50cb69e6ed
SoufianeKnadry <Soufianeknad@gmail.com>
2025-02-16 16:28:30 -0500

    Add linter ignore rules for generated file

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -2,11 +2,22 @@ import globals from "globals";
 import pluginJs from "@eslint/js";
 import tseslint from "typescript-eslint";
 
-
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  {files: ["**/*.{js,mjs,cjs,ts}"]},
-  {languageOptions: { globals: globals.browser }},
+  {
+    files: ["**/*.{js,mjs,cjs,ts}"],
+  },
+  {
+    languageOptions: { globals: globals.browser },
+  },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
+  {
+    files: ["src/graphql/generated/*", "prisma/migrations/*"], 
+    rules: {
+      "no-unused-vars": "off", 
+      "@typescript-eslint/no-empty-object-type": "off",  
+      "@typescript-eslint/no-explicit-any": "off",      
+    },
+  },
 ];
