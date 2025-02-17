b9788f32e603edb21a58eb4fb720143e32acab19
SoufianeKnadry <Soufianeknad@gmail.com>
2025-02-17 02:37:34 -0500

    Add test for completed input

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -26,5 +26,69 @@ describe("GraphQL getTodos", () => {
     expect(response.body.data.getTodos).to.be.an("array");
   });
 
+  // Test: Fetch todos with input filtering for completed: false
+  it("should fetch only todos with completed set to false", async () => {
+    const query = `
+      query GetTodos($completed: Boolean, $page: Int, $limit: Int, $sortOrder: String) {
+        getTodos(input: { completed: $completed, page: $page, limit: $limit, sortOrder: $sortOrder }) {
+          id
+          title
+          completed
+          createdAt
+          updatedAt
+        }
+      }
+    `;
+    const variables = {
+      completed: false,
+      page: 1,
+      limit: 10,
+      sortOrder: "asc"
+    };
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query, variables })
+      .expect(200);
+
+    const todos = response.body.data.getTodos;
+    expect(todos).to.be.an("array");
+    todos.forEach((todo: { completed: boolean }) => {
+      return expect(todo.completed).to.be.false; 
+    });
+  });
+
+  // Test: Fetch todos with input filtering for completed: true
+  it("should fetch only todos with completed set to true", async () => {
+    const query = `
+      query GetTodos($completed: Boolean, $page: Int, $limit: Int, $sortOrder: String) {
+        getTodos(input: { completed: $completed, page: $page, limit: $limit, sortOrder: $sortOrder }) {
+          id
+          title
+          completed
+          createdAt
+          updatedAt
+        }
+      }
+    `;
+    const variables = {
+      completed: true,
+      page: 1,
+      limit: 10,
+      sortOrder: "asc"
+    };
+
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query, variables })
+      .expect(200);
+
+    const todos = response.body.data.getTodos;
+    expect(todos).to.be.an("array");
+    todos.forEach((todo: { completed: boolean }) => {
+      return expect(todo.completed).to.be.true;
+    });
+  });
+
   
 });
