37a2170a7ab671ece221d0cfec99c3d8ff318510
SoufianeKnadry <Soufianeknad@gmail.com>
2025-02-17 02:35:12 -0500

    Add test to get all todos

--- tests/todo.spec.ts
+++ tests/todo.spec.ts
@@ -1,7 +1,30 @@
 import { expect } from "chai";
-import request from "supertest"; 
+import request from "supertest";
 import { app } from "../src/app";
 
-describe('GraphQL getTodos', () => {
+describe("GraphQL getTodos", () => {
+  // Test: Fetch all todos when no input is provided (input is null)
+  it("should fetch all todos when input is null", async () => {
+    const query = `
+      query {
+        getTodos(input: null) {
+          id
+          title
+          completed
+          createdAt
+          updatedAt
+        }
+      }
+    `;
 
+    const response = await request(app)
+      .post("/graphql")
+      .send({ query })
+      .expect(200);
+
+    expect(response.body.data).to.have.property("getTodos");
+    expect(response.body.data.getTodos).to.be.an("array");
+  });
+
+  
 });
